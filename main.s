0b8841406fd8489650092942bb8b5c8152e45db2
mdo21 <mdo21@skbl-418-006.ph.ic.ac.uk>
2023-11-28 15:59:05 +0000

    Edits

--- main.s
+++ main.s
@@ -14,13 +14,7 @@ delay_count:ds 1    ; reserve one byte for counter in the delay routine
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
 
-;psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-;myTable:
-;	db	'H','e','l','l','o',' ','W','o','o','r','l','d','!',0x0a
-					; message, plus carriage return
-;	myTable_l   EQU	14	; length of data
-;	align	2
+
 
 
     
@@ -44,46 +38,11 @@ start:
 	bra	start
 	
 delay:
-    movlw   0x0F
-    movwf   delay_count, A
-    decfsz  delay_count
-    bra	    delay
+	movlw   0x0F
+	movwf   delay_count, A
+	decfsz  delay_count
+	bra	    delay
+	return  
     
-    return  
-    
-	
-; ******* Main programme ****************************************
-;start: 	
-;	lfsr	0, myArray	; Load FSR0 with address in RAM	
-;	movlw	low highword(myTable)	; address of data in PM
-;	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-;	movlw	high(myTable)	; address of data in PM
-;	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-;	movlw	low(myTable)	; address of data in PM
-;	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-;	movlw	myTable_l	; bytes to read
-;	movwf 	counter, A		; our counter register
-;loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-;	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-;	decfsz	counter, A		; count down to zero
-;	bra	loop		; keep going until finished
-		
-;	movlw	myTable_l	; output message to UART
-;	lfsr	2, myArray
-;	call	KEY_Transmit_Message
-
-;	movlw	myTable_l	; output message to LCD
-;	addlw	0xff		; don't send the final carriage return to LCD
-;	lfsr	2, myArray
-;	call	LCD_Write_Message
-
-	;goto	$		; goto current line in code
-	
-	
-
-	; a delay subroutine if you need one, times around loop in delay_count
-;delay:	decfsz	delay_count, A	; decrement until zero
-;	bra	delay
-;	return
 
 	end	rst
