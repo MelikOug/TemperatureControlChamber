e1064469d1a119b9734bde851dd2ca243cbb45de
44738 <44738@DESKTOP-R0Q9PSD.home>
2023-12-01 22:32:42 +0000

    I2C write to sensor to set to normal mode

--- I2C.s
+++ I2C.s
@@ -4,6 +4,9 @@
 
 global I2C_Setup, I2C_Set_Sensor_On, I2C_Read_Pixel
     
+psect	udata_acs	;reserve data in acess RAM 
+I2D_Data: ds 1
+    
 psect	I2C_code,class=CODE
     
 I2C_Setup:    
@@ -59,16 +62,23 @@ I2C_Read_Pixel:
     call    check_AckR
     call    check_int
     
-    bsf     SSP1CON2, 2	    ;Generate  Repeated Start (RSEN) condition
+    bsf     SSP1CON2, 1	    ;Generate  Repeated Start (RSEN) condition
     call    check_int
     movlw   110100011B	    ;MS7Bits = slave address, LSB = 1 = Read
     movwf   SSP1BUF
     call    check_AckR
     call    check_int
     
-    bsf	    SSP1CON2, 3	    ;Reception mode enabled
+    bsf	    SSP1CON2, 3	    ;Reception mode (RCEN) enabled
+    call    check_int
+    movff   SSP1BUF, I2C_Data, A    ;Moves received data to var
+    bsf	    SSP1CON2, 5     ;Sets ACKDT Bit (NACK) (NACK only for final)
+    bsf	    SSP1CON2, 4	    ;Sets ACKEN bit to transmit ACKDT to slave
+    call    check_int
     
-    
+    bsf	    SSP1CON2, 2	    ;Generate stop condition
+    call    check_int
+    return
     
     
     
@@ -84,3 +94,7 @@ check_AckR:
     bra check_AckR
     return
     
+check_AckS:
+    btfsc SSP1CON2, 6; check if ack was received by slave (0 if received)
+    bra check_AckR
+    return
