3c9674594e8c473b775562ee9959bcd783542567
44738 <44738@DESKTOP-R0Q9PSD.home>
2023-12-01 21:21:12 +0000

    I2C write to sensor to set to normal mode

--- I2C.s
+++ I2C.s
@@ -7,97 +7,65 @@ global I2C_Setup, I2C_Write_Test
 psect	I2C_code,class=CODE
     
 I2C_Setup:    
-    call Mode_Config
-    call Bit_Rate_Setup
-    call Slew_Rate_Control
-    call Pin_Setup
-    return
-
-I2C_Write_Test:
-    movlw 11010000B ; load value
-    ; into W
-    movwf SSP1BUF ; initiate I2C
-    ; write cycle
-    ; This code checks for completion of I2C
-    ; write event
-    btfsc SSP1STAT, 2 ; test write bit (R/W bit)
-; state
-    goto $-1 ; module busy
+    movlw 00101000B 
+    movwf SSP1CON1 ; config for master mode
     
+    movlw 0x27
+    movwf SSP1ADD ; baud rate = 400KHz @ 16MHz
     
-I2C_Read_Event:
-    ; bank
-    bsf SSP1CON2,3 ; initiate (RCEN Bit)
-    ; I2C read
-    ; This code checks for completion of I2C
-    ; read event
-    btfsc SSP1CON2,3 ; test read(RCEN Bit)
-    ; bit state
-    goto $-1 ; module busy
-    ; so wait
-    return
-    
-
-    
-Mode_Config:
-    movlw 00101000B ; setup value
-    ; into W register
-    banksel SSP1CON1 ; select SFR
-    ; bank
-    movwf SSP1CON1 ; configure for
-    ; Master I2C
-    return
-    
-Bit_Rate_Setup:
-    movlw 0x27 ; setup value
-    ; into W register
-    movwf SSP1ADD ; baud rate =
-    ; 400KHz @ 16MHz
-    return
-    
-Slew_Rate_Control:
-    movlw 00000000B ; setup value
-    ; into W register
+    movlw 00000000B 
     movwf SSP1STAT ; slew rate
-    ; enabled
+    
+    movlw 00011000B
+    iorwf TRISC,F ; SCL and SDA are inputs
+    
     return
- 
-Pin_Setup:
-    movlw 00011000B ; setup value
-    ; into W register
-    iorwf TRISC,F ; SCL and SDA
-    ; are inputs
+
+I2C_Set_Sensor_On: 
+    ;Grid-EYE_AMG88X_I2C communication (p2)
+    ;21.4.6.1 I2C? Master Mode Operation (p312)
+    
+    bsf	    SSP1CON2, 0	    ;Generate start condition
+    ;SSPxIF is set (bsf PIR1, 3 )
+    ;The MSSP module will wait the required start time
+    ;before any other operation takes place
+    call    check_int
+    movlw   110100010B	    ;MS7Bits = slave address, LSB = 0 = Write
+    movwf   SSP1BUF
+    call    check_AckR
+    call    check_int
+    movlw   0x00	    ;Power control register address
+    movwf   SSP1BUF
+    call    check_AckR
+    call    check_int
+    movlw   0x00	    ;Set sensor to normal mode
+    movwf   SSP1BUF
+    call    check_AckR
+    call    check_int
+    bsf	    SSP1CON2, 2	    ;Generate stop condition
+    call    check_int
     return
+    
+check_int:
+    btfss PIR1, 3    ;is start condition finished setting up
+    bra check_int
+    bcf	  PIR1, 3    ;must be cleared by software
+    return
+    
+check_AckR:
+    btfsc SSP1CON2, 6; check if ack was received by slave (0 if received)
+    bra check_AckR
+    return
+    
+
+
     
 Ack_Event:
-    ; bank
-    bcf SSP1CON2, 5 ; set (ACKDT) bit
-    ; state to 0
-    bsf SSP1CON2, 4 ; initiate ack (ACKEN)
+    bcf SSP1CON2, 5 ; set (ACKDT) bit  to 0
+    bsf SSP1CON2, 4 ; set (ACKEN) bit to 1
     return
     
-Not_Ack_Event:
-    ; bank
-    bsf SSP1CON2, 5 ; set ack bit
-    ; state to 1
-    bsf SSP1CON2, 4 ; initiate ack
+NAck_Event:
+    bsf SSP1CON2, 5 ; set (ACKDT) bit to 1
+    bsf SSP1CON2, 4 ; set (ACKEN) bit to 0
     return
-    
-Idle_Check:
-    ;i2c_idle ; routine name
-    ; bank
-    btfsc SSP1STAT,2 ; transmit (R/W)
-    ; in progress?
-    goto $-1 ; module busy
-    ; so wait
-    ; bank
-    movf SSP1CON2,w ; get copy
-    ; of SSPCON2
-    andlw 0x1F ; mask out
-    ; non-status
-    btfss STATUS,0 ; test for
-    ; zero state
-    goto $-3 ; bus is busy
-    ; test again
-    return ; return
-
