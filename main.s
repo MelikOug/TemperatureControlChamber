d00a64b7df85ce45d2b7d23038659c25af87a6db
44738 <44738@192.168.1.193>
2023-12-04 19:13:48 +0000

    Ready to test in Lab

--- main.s
+++ main.s
@@ -6,17 +6,6 @@ extrn	UART_Setup, UART_Transmit_Message
 extrn	I2C_Setup, I2C_Set_Sensor_On, I2C_Read_Pixels
 ;extrn	External_Setup
     
-global delay
-	
-psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
-delay_count:ds 1    ; reserve one byte for counter in the delay routine
-
-
-
- 
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
 
 
 
@@ -51,18 +40,9 @@ External_Setup:
 	
 start:
 	call	KEY_Read_Message
-	;call	delay
 	;call	I2C_Read_Pixels
 	call	UART_Transmit_Message
 	call	LCD_Update
 	bra	start
 	
-delay:
-	movlw   0x0F
-	movwf   delay_count, A
-	decfsz  delay_count, A
-	bra	delay
-	return  
-    
-
 	end	rst
