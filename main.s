49513873924609a5c3979037cadea7c2c7e61d22
mdo21 <mdo21@skbl-418-006.ph.ic.ac.uk>
2023-12-05 15:59:10 +0000

    I2C

--- main.s
+++ main.s
@@ -3,7 +3,8 @@
 extrn	KEY_Setup, KEY_Read_Message  ; external subroutines
 extrn	LCD_Setup, LCD_Update
 extrn	UART_Setup, UART_Transmit_Message
-extrn	I2C_Setup, I2C_Set_Sensor_On, I2C_Read_Pixels
+extrn	I2C_Setup, I2C_Set_Sensor_On, I2C_Read_Pixels, check_int
+    
 ;extrn	External_Setup
     
 
@@ -15,26 +16,27 @@ psect	code, abs
 rst: 	org 0x0
  	goto	setup
 
+int_hi: org 0x0008
+        movlw 0x88
+        movwf  LATE, A 
+	retfie f
+ ;goto check_int
+	
+    org	0x100		
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
-	;call	I2C_Setup
-	;call	I2C_Set_Sensor_On
+	;	call	External_Setup
+	call	I2C_Setup
+	call	I2C_Set_Sensor_On
+	call	I2C_Read_Pixels
 	call	LCD_Setup	; setup LCD
 	call	UART_Setup
 	call	KEY_Setup	; setup KeyPad
-	call	External_Setup
+	
 	goto	start
 	
-External_Setup:
-	clrf    LATJ
-	movlw   0x00
-	movwf   TRISJ,A
-	bsf	LATJ, 0, A
-	bcf	LATJ, 1, A
-	bsf	LATJ, 2, A
-	
-	return
+
     
 	
 	
