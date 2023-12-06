c5f82d060bd769e63d32e99c44b0419e92720f16
44738 <44738@DESKTOP-R0Q9PSD.home>
2023-12-06 17:48:25 +0000

    Changed so that each pair of low and high byte data sent spaced by 1 second

--- UART.s
+++ UART.s
@@ -1,9 +1,11 @@
 #include <xc.inc>
     
-global  UART_Setup, UART_Transmit_Message, Pixel_Data
+global  UART_Setup, UART_Transmit_Pixels, Pixel_Data, Pixel_Counter
+extrn	LCD_delay_ms
 
 psect	udata_acs   ; reserve data space in access ram
 UART_counter: ds    1	    ; reserve 1 byte for variable UART_counter
+Pixel_Counter:ds    1
 
 psect	uart_code,class=CODE
 UART_Setup:
@@ -18,16 +20,25 @@ UART_Setup:
 					; must set TRISC6 to 1
     return
 
-UART_Transmit_Message:	    ; Message stored at FSR2, length is 128
+UART_Transmit_Pixels:	    ; Message stored at FSR2, length is 128
     movlw 128
     movwf   UART_counter, A
+    movlw   3
+    movwf   Pixel_Counter, A; 3-1 = 2
     lfsr    0, Pixel_Data
 UART_Loop_message:
-    movf    POSTINC0, W, A  ; Moves data stored at address in FSR0 to WR
+    decfsz  Pixel_Counter, A
+    bra	    Continue
+    call    Pixel_Delay
+    bra	    Continue
+    
+Continue:
+    movf    POSTINC0, W, A  ;Moves data stored at address in FSR0 to WR
     call    UART_Transmit_Byte
     decfsz  UART_counter, A
     bra	    UART_Loop_message
     return
+    
 
 UART_Transmit_Byte:	    ; Transmits byte stored in W
     btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
@@ -35,4 +46,10 @@ UART_Transmit_Byte:	    ; Transmits byte stored in W
     movwf   TXREG1, A
     return
 
-
+    
+Pixel_Delay:
+    movlw   1000
+    call    LCD_delay_ms
+    movlw   2
+    movwf   Pixel_Counter	;reset pixel counter 
+    Return
