42e08aba488c8f00f2521dbba2792a6181f0d595
44738 <44738@192.168.1.206>
2023-12-10 12:19:48 +0000

    Indicates mode on LCD

--- External.s
+++ External.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
     
 global  External_Setup, External_Mode
-extrn	target, sum_high
+extrn	target, sum_high, LCD_Send_Byte_I, LCD_Send_Byte_D, LCD_delay_x4us, DDRAM_Address
 psect	external_code,class=CODE
 External_Setup:
     clrf    LATJ
@@ -18,26 +18,57 @@ External_Mode:
     
 check_thresh:
     decf    target, W, A	;Subtract 1 from target and store in WR
-    cpfsgt  sum_high, A	;If current value is greater than (target-1) (and less than target+1) turn off
-    bra	    heat	;If its less than (target-1), heat
+    cpfsgt  sum_high, A	;If current value is greater than (target-2) (and less than target+1) turn off
+    bra	    heat	;If its less than (target-2), heat
     bra	    off
     
 cool: 
     bcf	    LATJ, 0	;Direction  (0)
     bcf	    LATJ, 1	;Brake	    (0)
     bsf	    LATJ, 2	;Duty Cycle (1)
+    movlw   10001111B
+    call    LCD_Send_Byte_I
+    movlw   10
+    call    LCD_delay_x4us
+    movlw   'C'
+    call    LCD_Send_Byte_D
+    movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
+    call    LCD_Send_Byte_I
+    movlw   10		; wait 40us
+    call    LCD_delay_x4us
     return
     
 heat: 
     bsf	    LATJ, 0	;Direction  (1)
     bcf	    LATJ, 1	;Brake	    (0)
     bsf	    LATJ, 2	;Duty Cycle (1)
+    movlw   10001111B
+    call    LCD_Send_Byte_I
+    movlw   10
+    call    LCD_delay_x4us
+    movlw   'H'
+    call    LCD_Send_Byte_D
+    movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
+    call    LCD_Send_Byte_I
+    movlw   10		; wait 40us
+    call    LCD_delay_x4us
     return
     
 off:
     bcf	    LATJ, 0	;Direction  (0)
     bsf	    LATJ, 1	;Brake	    (1)
     bsf	    LATJ, 2	;Duty Cycle (1)
+    movlw   10001111B
+    call    LCD_Send_Byte_I
+    movlw   10
+    call    LCD_delay_x4us
+    movlw   'O'
+    call    LCD_Send_Byte_D
+    
+    movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
+    call    LCD_Send_Byte_I
+    movlw   10		; wait 40us
+    call    LCD_delay_x4us
     return
 
 
