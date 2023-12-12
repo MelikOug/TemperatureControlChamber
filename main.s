67f66f2f547be54bc7c29529a095431d824d162f
44738 <44738@192.168.1.206>
2023-12-12 21:40:38 +0000

    Commented

--- main.s
+++ main.s
@@ -1,39 +1,35 @@
 #include <xc.inc>
-
-extrn	KEY_Setup, KEY_Read_Message  ; external subroutines
+    
+;External subroutines called by the main.s file
+extrn	KEY_Setup, KEY_Read_Message  
 extrn	LCD_Setup, LCD_Update
 extrn	UART_Setup, UART_Transmit_Pixels
 extrn	I2C_Setup, I2C_Set_Sensor_On, I2C_Read_Pixels, I2C_Average_Pixels
 extrn	External_Setup, External_Mode
-    
-
-
-
-
+        
     
 psect	code, abs	
 rst: 	org 0x0
  	goto	setup
-
 		
-	org 0x100
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	call	External_Setup
-	call	I2C_Setup
-	call	I2C_Set_Sensor_On
-	call	LCD_Setup	; setup LCD
-	call	UART_Setup
-	call	KEY_Setup	; setup KeyPad
+	org	0x100			;The address in PM of where the instructions start storing
+setup:	bcf	CFGS			;Point to Flash program memory  
+	bsf	EEPGD			;Access Flash program memory
+	call	External_Setup		;Configures PORTJ that will control the Heating Module 
+	call	I2C_Setup		;Configures the I2C protocol that will be used to read data from the temperature sensor
+	call	I2C_Set_Sensor_On	;Turns on the temperature sensor
+	call	LCD_Setup		;Configures the LCD Screen
+	call	UART_Setup		;Configures the UART protocol which will be used to send sensor data to PC
+	call	KEY_Setup		;Configures PORTE and variables required to read data from keypad
 	goto	start
 
 start:
-	call	KEY_Read_Message
-	call	I2C_Read_Pixels
-	call	UART_Transmit_Pixels
-	call	I2C_Average_Pixels
-	call	LCD_Update
-	call	External_Mode
+	call	KEY_Read_Message	;Reads message inputted on keypad
+	call	I2C_Read_Pixels		;Reads data from each pixel on temperature sensor to microcontroller via I2C
+	call	UART_Transmit_Pixels	;Sends this data to PC
+	call	I2C_Average_Pixels	;Calculates the mean temperature across all pixels
+	call	LCD_Update		;Updates LCD screen with necessary information
+	call	External_Mode		;Sets the mode of the heating module depending parameters
 	bra	start
 	
 	end	rst
