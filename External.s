4fda83cc69f32903981103a9e55a29a31da7b83d
44738 <44738@192.168.1.213>
2023-12-14 20:31:40 +0000

    Made changes so threshold is the only the target temperature. Need to test.

--- External.s
+++ External.s
@@ -5,29 +5,29 @@ global  External_Setup, External_Mode
 extrn	target, sum_high, DDRAM_Address
 extrn	LCD_Send_Byte_I, LCD_Send_Byte_D, LCD_delay_x4us
 psect	external_code,class=CODE
+    
 External_Setup:
     clrf    LATJ
     movlw   0x00
-    movwf   TRISJ
+    movwf   TRISJ		;Set all pins to output
+    bsf	    LATJ, 2		;Duty Cycle (1)
     
 External_Mode:
     ;Compare current(sum_high) with target
+    movf    target, W	
     
-    incf    target, W, A	;Add 1 to target and store in WR
-    cpfsgt  sum_high, A	;If current value is greater than (target+1), cool
-    bra	    check_thresh;If it is not greater than upper threshold, check if in threshold
+    cpfsgt  sum_high, A		;If current value is greater than (target), cool
+    bra	    check_heat		;If it is not greater than target, check if need to heat
     bra	    cool
     
-check_thresh:
-    decf    target, W, A	;Subtract 1 from target and store in WR
-    cpfsgt  sum_high, A	;If current value is greater than (target-2) (and less than target+1) turn off
-    bra	    heat	;If its less than (target-2), heat
-    bra	    off
+check_heat:			
+    cpfslt  sum_high, A		;If current value is less than target, heat
+    bra	    off			;If is not greater but not less than, turn off
+    bra	    heat
     
 cool: 
-    bsf	    LATJ, 0	;Direction  (0)
-    bcf	    LATJ, 1	;Brake	    (0)
-    bsf	    LATJ, 2	;Duty Cycle (1)
+    bsf	    LATJ, 0		;Direction  (0)
+    bcf	    LATJ, 1		;Brake	(0)
     movlw   10001111B
     call    LCD_Send_Byte_I
     movlw   10
@@ -36,14 +36,13 @@ cool:
     call    LCD_Send_Byte_D
     movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
     call    LCD_Send_Byte_I
-    movlw   10		; wait 40us
+    movlw   10			;wait 40us
     call    LCD_delay_x4us
     return
     
 heat: 
-    bcf	    LATJ, 0	;Direction  (1)
-    bcf	    LATJ, 1	;Brake	    (0)
-    bsf	    LATJ, 2	;Duty Cycle (1)
+    bcf	    LATJ, 0		;Direction  (1)
+    bcf	    LATJ, 1		;Brake	(0)
     movlw   10001111B
     call    LCD_Send_Byte_I
     movlw   10
@@ -52,14 +51,13 @@ heat:
     call    LCD_Send_Byte_D
     movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
     call    LCD_Send_Byte_I
-    movlw   10		; wait 40us
+    movlw   10			;wait 40us
     call    LCD_delay_x4us
     return
     
 off:
-    bcf	    LATJ, 0	;Direction  (0)
-    bsf	    LATJ, 1	;Brake	    (1)
-    bsf	    LATJ, 2	;Duty Cycle (1)
+    bcf	    LATJ, 0		;Direction  (0)
+    bsf	    LATJ, 1		;Brake	(1)
     movlw   10001111B
     call    LCD_Send_Byte_I
     movlw   10
@@ -69,7 +67,7 @@ off:
     
     movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
     call    LCD_Send_Byte_I
-    movlw   10		; wait 40us
+    movlw   10			;wait 40us
     call    LCD_delay_x4us
     return
 
