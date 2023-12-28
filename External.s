4f7736f0e5680db32f64e36b125e0132036d7417
44738 <44738@DESKTOP-R0Q9PSD.home>
2023-12-28 17:45:07 +0000

    Fully Commented!

--- External.s
+++ External.s
@@ -13,15 +13,17 @@ External_Setup:
     bsf	    LATJ, 2		;Duty Cycle (1)
     
 External_Mode:
-    ;Compare current(sum_high) with target
-    movf    target, W	
+    ;Compare current temperature (sum_high value) (above the decimal point) with target
+    ;Leaves peltier off when within range of 1 degree C either side of sum_high
+    movf    target, W, A	
     
-    cpfsgt  sum_high, A		;If current value is greater than (target), cool
+    cpfsgt  sum_high, A		;If sum_high value is greater than (target), cool
     bra	    check_heat		;If it is not greater than target, check if need to heat
     bra	    cool
     
-check_heat:			
-    cpfslt  sum_high, A		;If current value is less than target, heat
+check_heat:	
+    decf    target, W, A
+    cpfslt  sum_high, A		;If sum_high value is less than target-1, heat
     bra	    off			;If is not greater but not less than, turn off
     bra	    heat
     
@@ -29,45 +31,45 @@ cool:
     bsf	    LATJ, 0		;Direction  (0)
     bcf	    LATJ, 1		;Brake	(0)
     movlw   10001111B
-    call    LCD_Send_Byte_I
+    call    LCD_Send_Byte_I	;Sets DDRAM address to bottom right square
     movlw   10
-    call    LCD_delay_x4us
+    call    LCD_delay_x4us	;wait 40us
     movlw   'C'
-    call    LCD_Send_Byte_D
+    call    LCD_Send_Byte_D	;writes 'C' to LCD to indicate cooling
     movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
-    call    LCD_Send_Byte_I
-    movlw   10			;wait 40us
-    call    LCD_delay_x4us
+    call    LCD_Send_Byte_I	;revert DDRAM address back to what it was before
+    movlw   10			
+    call    LCD_delay_x4us	;wait 40us
     return
     
 heat: 
     bcf	    LATJ, 0		;Direction  (1)
     bcf	    LATJ, 1		;Brake	(0)
     movlw   10001111B
-    call    LCD_Send_Byte_I
+    call    LCD_Send_Byte_I	;Sets DDRAM address to bottom right square
     movlw   10
-    call    LCD_delay_x4us
+    call    LCD_delay_x4us	;wait 40us
     movlw   'H'
-    call    LCD_Send_Byte_D
-    movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
-    call    LCD_Send_Byte_I
-    movlw   10			;wait 40us
-    call    LCD_delay_x4us
+    call    LCD_Send_Byte_D	;writes 'H' to LCD to indicate heating
+    movf    DDRAM_Address, W, A 
+    call    LCD_Send_Byte_I	;revert DDRAM address back to what it was before
+    movlw   10			
+    call    LCD_delay_x4us	
     return
     
 off:
     bcf	    LATJ, 0		;Direction  (0)
     bsf	    LATJ, 1		;Brake	(1)
     movlw   10001111B
-    call    LCD_Send_Byte_I
+    call    LCD_Send_Byte_I	;Sets DDRAM address to bottom right square
     movlw   10
-    call    LCD_delay_x4us
+    call    LCD_delay_x4us	;writes 'O' to LCD to indicate peltier is off
     movlw   'O'
     call    LCD_Send_Byte_D
     
-    movf    DDRAM_Address, W, A ;Address after target: (either 1st or 2nd)
-    call    LCD_Send_Byte_I
-    movlw   10			;wait 40us
+    movf    DDRAM_Address, W, A 
+    call    LCD_Send_Byte_I	;revert DDRAM address back to what it was before
+    movlw   10			
     call    LCD_delay_x4us
     return
 
