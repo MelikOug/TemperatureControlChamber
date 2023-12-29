7d0004588f1eb7a54bc0994e3da73b5acb6c6616
MelikOug <86121388+MelikOug@users.noreply.github.com>
2023-12-29 10:33:47 +0000

    TCC Analysis.py

--- TCC Analysis.py
+++ TCC Analysis.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+temp = str(23)
 clock, meanT, minT, maxT, std = np.loadtxt(
-    'C:\\Users\\mdo21\\OneDrive - Imperial College London\\Documents\\TCC Osc Data\\TCC_Data_Osc20.txt', unpack=True, dtype=str)
+    'C:\\Users\\44738\\OneDrive - Imperial College London\\Documents\\TCC Osc Data\\TCC_Data_Osc'+temp+'.txt', unpack=True, dtype=str)
 hours, minutes, seconds = np.array(
     [time.split(':') for time in clock], dtype=int).T
 total_seconds = (hours * 3600) + (minutes*60) + seconds
@@ -14,7 +15,7 @@ std = std.astype(float)
 
 
 clock2, PTemp = np.loadtxt(
-    'Peltier_Osc20.txt', unpack=True, usecols=(1, 3), dtype=str, skiprows=1)
+    'C:\\Users\\44738\\OneDrive - Imperial College London\\Documents\\Peltier Osc Data\\Peltier_Osc'+temp+'.txt', unpack=True, usecols=(1, 3), dtype=str, skiprows=1)
 clock2 = np.array([date[-8:] for date in clock2])
 PTemp = PTemp.astype(float)
 hours, minutes, seconds = np.array(
@@ -37,6 +38,10 @@ ax.errorbar(total_seconds_shifted, meanT, ls='-',
 ax.plot(total_seconds_shifted, minT, ls='-',
         marker='.', label='Minimum Temperature')
 ax.plot(total_seconds_shifted, maxT, ls='-',
+        marker='.', label='Maximum Temperature')
+ax.plot(total_seconds2_shifted, PTemp, label='Peltier Temperature')
+#ax.plot(seconds, std, ls = '-', marker = '.',label = "$\sigma$")
+ax.legend()
         marker='.', label='Maximum Temperature')
 ax.plot(total_seconds2_shifted, PTemp, label='Peltier Temperature')
 #ax.plot(seconds, std, ls = '-', marker = '.',label = "$\sigma$")
